d8e23c26cd70a4a343d03874a7184bd5217d116c
the-rene <rene@magritz.com>
2020-01-10 00:20:37 +0100

    update repo in setup

--- tools/install.sh
+++ tools/install.sh
@@ -33,7 +33,7 @@ set -e
 
 # Default settings
 ZSH=${ZSH:-~/.oh-my-zsh}
-REPO=${REPO:-ohmyzsh/ohmyzsh}
+REPO=${REPO:-the-rene/ohmyzsh}
 REMOTE=${REMOTE:-https://github.com/${REPO}.git}
 BRANCH=${BRANCH:-master}
 
